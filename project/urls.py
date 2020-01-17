50c009597f0a65959e6e54a5889cf5fd326d19b4
Rodrigo Durante <55359428+engineerGuigo@users.noreply.github.com>
2020-01-17 00:44:17 -0200

    "Detail view added for each blog post."

--- project/urls.py
+++ project/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 127.0.0.1:8000
     path('', views.post_list, name='post_list'),
+    
+    # 127.0.01:8000/post/2
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
